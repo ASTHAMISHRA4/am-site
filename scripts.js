fa41297b41d02e7caaaa565299ccf1618a319e05
ASTHAMISHRA4 <52097499+ASTHAMISHRA4@users.noreply.github.com>
2021-03-19 01:09:52 +0530

    Added animation

--- scripts.js
+++ scripts.js
@@ -60,6 +60,43 @@ $(window).on('load', function () {
     shockable:4
 });
 
+//-----------------SMOOTH SCROLL----------//
+$(".home-btn").click(function() {
+  $('html,body').animate({
+      scrollTop: $("#home").offset().top},
+      'slow');
+});
+$(".about-btn").click(function() {
+  $('html,body').animate({
+      scrollTop: $("#about").offset().top},
+      'slow');
+});
+$(".education-btn").click(function() {
+  $('html,body').animate({
+      scrollTop: $("#education").offset().top},
+      'slow');
+});
+$(".skills-btn").click(function() {
+  $('html,body').animate({
+      scrollTop: $("#skills").offset().top},
+      'slow');
+});
+$(".experience-btn").click(function() {
+  $('html,body').animate({
+      scrollTop: $("#experience").offset().top},
+      'slow');
+});
+$(".blog-btn").click(function() {
+  $('html,body').animate({
+      scrollTop: $("#blog").offset().top},
+      'slow');
+});
+$(".contact-btn").click(function() {
+  $('html,body').animate({
+      scrollTop: $("#contact").offset().top},
+      'slow');
+});
+
 });
 
 //---------TYPING ANIMATION---------//
@@ -214,10 +251,49 @@ function offOverlay(id)
 //---------PROFILE PICTURE ANIMATION---------//
 function onHover(id,img)
 {
-    $(id).attr('src', img);
+    $('#'+id).attr('src', img);
 }
 
 function offHover(id,img)
 {
-    $(id).attr('src', img);
+    $('#'+id).attr('src', img);
 }
+function onHoverMove(id,img)
+{
+    onHover(id,img)
+    document.getElementById(id).style.transform = "scaleX(-1)";
+}
+
+function offHoverMove(id,img)
+{
+    offHover(id,img);
+    document.getElementById(id).style.transform = "scaleX(1)";
+}
+
+//---------------------ANIMATION SLIDE LEFT---------------------//
+var $animation_elements = $('.animation-element');
+var $window = $(window);
+
+function check_if_in_view() {
+  var window_height = $window.height();
+  var window_top_position = $window.scrollTop();
+  var window_bottom_position = (window_top_position + window_height);
+ 
+  $.each($animation_elements, function() {
+    var $element = $(this);
+    var element_height = $element.outerHeight();
+    var element_top_position = $element.offset().top;
+    var element_bottom_position = (element_top_position + element_height);
+ 
+    //check to see if this current container is within viewport
+    if ((element_bottom_position >= window_top_position) &&
+        (element_top_position <= window_bottom_position)) {
+      $element.addClass('in-view');
+    } else {
+      $element.removeClass('in-view');
+    }
+  });
+}
+
+$window.on('scroll resize', check_if_in_view);
+$window.trigger('scroll');
